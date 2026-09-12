d91024d25cb2bfb4f6c6691d7c0702d7cf4b208f
MysterioCrypto <144173232+MysterioCrypto@users.noreply.github.com>
2026-09-12 11:14:04 +0500

    Expose terrain generation stage API

--- src/domain_generator/terrain/__init__.py
+++ src/domain_generator/terrain/__init__.py
@@ -1,3 +1,17 @@
+from .generate import (
+    TerrainCapabilityError,
+    generate_terrain,
+    rasterize_area_cell_centers,
+    terrain_stage,
+    validate_terrain,
+)
 from .state import TerrainState
 
-__all__ = ["TerrainState"]
+__all__ = [
+    "TerrainCapabilityError",
+    "TerrainState",
+    "generate_terrain",
+    "rasterize_area_cell_centers",
+    "terrain_stage",
+    "validate_terrain",
+]
