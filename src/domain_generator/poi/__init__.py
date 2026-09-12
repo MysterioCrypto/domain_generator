d9521cb282822bf2bf3f40dacbaae0a27d15cb91
MysterioCrypto <144173232+MysterioCrypto@users.noreply.github.com>
2026-09-12 15:20:23 +0500

    Export dependent placement candidate filtering API

--- src/domain_generator/poi/__init__.py
+++ src/domain_generator/poi/__init__.py
@@ -1,5 +1,14 @@
 """Dependent placement runtime primitives."""
 
+from .candidates import (
+    EvaluatedSite,
+    PlacementCandidateCapabilityError,
+    evaluate_candidate_sites,
+    filter_valid_sites,
+    generate_candidate_points,
+    generate_valid_sites_for_feature,
+    requirement_passes,
+)
 from .site_metrics import (
     SITE_METRIC_IDS,
     SiteMetricCapabilityError,
@@ -9,9 +18,16 @@ from .site_metrics import (
 )
 
 __all__ = [
+    "EvaluatedSite",
+    "PlacementCandidateCapabilityError",
     "SITE_METRIC_IDS",
     "SiteMetricCapabilityError",
     "SiteMetricContext",
+    "evaluate_candidate_sites",
     "evaluate_site_metrics",
+    "filter_valid_sites",
     "footprint_cells",
+    "generate_candidate_points",
+    "generate_valid_sites_for_feature",
+    "requirement_passes",
 ]
