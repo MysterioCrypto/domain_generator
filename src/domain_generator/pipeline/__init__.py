f3b74ae0727369e7436fae6d0b303911e10da150
MysterioCrypto <144173232+MysterioCrypto@users.noreply.github.com>
2026-09-12 18:50:51 +0500

    Export final validation stage

--- src/domain_generator/pipeline/__init__.py
+++ src/domain_generator/pipeline/__init__.py
@@ -15,6 +15,7 @@ from .attempts import (
     run_generation,
     validation_passed,
 )
+from .final import FinalValidationCapabilityError, final_stage, validate_final
 from .rng import RngFactory, RngKey, RngStage, Xoshiro256StarStar
 
 __all__ = [
@@ -22,6 +23,7 @@ __all__ = [
     "AttemptContext",
     "CandidateState",
     "DomainCandidate",
+    "FinalValidationCapabilityError",
     "GenerationFailure",
     "GenerationRunResult",
     "PipelineInvariantError",
@@ -32,7 +34,9 @@ __all__ = [
     "StageStep",
     "Xoshiro256StarStar",
     "candidate_rank_key",
+    "final_stage",
     "run_attempt",
     "run_generation",
+    "validate_final",
     "validation_passed",
 ]
